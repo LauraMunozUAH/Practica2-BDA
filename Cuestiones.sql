6f45c8ec647383858ba389d4fba681a2e2612431
LauraMunozUAH <l.m.centenera@gmail.com>
2026-03-09 11:08:36 +0100

    cuestion 2

--- Cuestiones.sql
+++ Cuestiones.sql
@@ -9,3 +9,37 @@ SELECT
     current_setting('log_line_prefix') AS log_line_prefix,
     current_setting('max_parallel_workers_per_gather') AS max_parallel_workers_per_gather,
     current_setting('max_parallel_workers') AS max_parallel_workers;
+
+--CUESTIÓN 2
+CREATE TABLE estudiantes (
+    carnet NUMERIC PRIMARY KEY,
+    nombre TEXT,
+    apellidos TEXT,
+    creditos NUMERIC
+);
+CREATE TABLE asignaturas (
+    codigo NUMERIC PRIMARY KEY,
+    nombre TEXT,
+    caracter TEXT,
+    creditos NUMERIC
+);
+CREATE TABLE matriculas (
+    carnet_estu NUMERIC,
+    codigo_asig NUMERIC,
+    nota NUMERIC,
+    PRIMARY KEY (carnet_estu, codigo_asig),
+    FOREIGN KEY (carnet_estu) REFERENCES estudiantes(carnet) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY (codigo_asig) REFERENCES asignaturas(codigo) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+COPY estudiantes(carnet,nombre, apellidos, creditos)
+FROM 'C:\datos_estudiantes.csv'
+WITH (DELIMITER ',', FORMAT csv, HEADER false);
+
+COPY asignaturas(codigo,nombre, caracter, creditos)
+FROM 'C:\datos_asignaturas.csv'
+WITH (DELIMITER ',', FORMAT csv, HEADER false);
+
+COPY matriculas(carnet_estu,codigo_asig, nota)
+FROM 'C:\datos_matriculas.csv'
+WITH (DELIMITER ',', FORMAT csv, HEADER false);
