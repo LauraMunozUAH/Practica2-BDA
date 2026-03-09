b7f25629821c89cff7b3ca4371dbf2bf07ca2dd5
inesperezpascual <ines.perezpascual@edu.uah.es>
2026-03-09 10:29:17 +0100

    Ej 0

--- Cuestiones.sql
+++ Cuestiones.sql
@@ -1,1 +1,11 @@
-ño-po.i,mngbffxcvbnmkjhgg
+-- CUESTIÓN 0
+SELECT pg_reload_conf();
+-- Mostrar todos los parámetros para comprobar si esta bien cambiado
+SELECT
+    current_setting('logging_collector') AS logging_collector,
+    current_setting('log_statement') AS log_statement,
+    current_setting('log_duration') AS log_duration,
+    current_setting('log_min_duration_statement') AS log_min_duration_statement,
+    current_setting('log_line_prefix') AS log_line_prefix,
+    current_setting('max_parallel_workers_per_gather') AS max_parallel_workers_per_gather,
+    current_setting('max_parallel_workers') AS max_parallel_workers;
